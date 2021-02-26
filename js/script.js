579f61e1d8c4ddf16be78db9fbe6ca2acd0660db
Joy Kirii <joykirii@gmail.com>
2021-02-26 12:05:19 +0300

    add toggle function to what we do icons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+    $(".click1").click(function() {
+        $(".content-hidden1").toggle();
+        $("#content-show1").hide();
+    })
+    $(".click2").click(function() {
+        $(".content-hidden2").toggle();
+        $("#content-show2").hide();
+    })
+    $(".click3").click(function() {
+        $(".content-hidden3").toggle();
+        $("#content-show3").hide();
+    })
 });
