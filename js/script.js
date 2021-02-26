b1876295fb4148db006ccc96182dd52febf902ed
Joy Kirii <joykirii@gmail.com>
2021-02-26 12:40:29 +0300

    tested toggle function on what we do section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
+ // what we do toggle
     $(".click1").click(function() {
+        $("#content-show1").toggle();
         $(".content-hidden1").toggle();
-        $("#content-show1").hide();
+        $(".content-hidden1").click(function() {
+            $("#content-show1").show();
+            $(".content-hidden1").hide();
+        })
+        // 
     })
     $(".click2").click(function() {
+        $("#content-show2").toggle();
         $(".content-hidden2").toggle();
-        $("#content-show2").hide();
+        $(".content-hidden2").click(function() {
+            $("#content-show2").show();
+            $(".content-hidden2").hide();
+        })
+        // $("#content-show2").hide();
     })
     $(".click3").click(function() {
+        $("#content-show3").toggle();
         $(".content-hidden3").toggle();
-        $("#content-show3").hide();
+        $(".content-hidden3").click(function() {
+            $("#content-show3").show();
+            $(".content-hidden3").hide();
+        })
+        // $("#content-show3").hide();
     })
 });
