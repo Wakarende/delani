f408dd661a94af53c87b2389cbde1247588dafac
Joy Kirii <joykirii@gmail.com>
2021-02-26 16:25:09 +0300

    add hover function to portfolio section

--- js/script.js
+++ js/script.js
@@ -3,31 +3,25 @@ $(document).ready(function() {
     $(".click1").click(function() {
         $("#content-show1").toggle();
         $(".content-hidden1").toggle();
-        $(".content-hidden1").click(function() {
-            // $("#content-show1").show();
-            // $(".content-hidden1").hide();
-        });
     });
     $(".click2").click(function() {
         $("#content-show2").toggle();
         $(".content-hidden2").toggle();
-        $(".content-hidden2").click(function() {
-            $("#content-show2").show();
-            $(".content-hidden2").hide();
-        });
-        // $("#content-show2").hide();
     });
     $(".click3").click(function() {
         $("#content-show3").toggle();
         $(".content-hidden3").toggle();
-        $(".content-hidden3").click(function() {
-            $("#content-show3").show();
-            $(".content-hidden3").hide();
-        })
-        // $("#content-show3").hide();
     });
 // portfolio hover
 
+    $(".card").hover(
+        function(){
+            $(".card-body").slideDown();
+        },
+     function(){
+         $(".card-body").slideUp();
+     }
+    );
    
 });
 
